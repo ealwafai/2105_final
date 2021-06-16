4881cd31276f963b0a5723843c3166d6ca3bd881
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-06-16 10:53:52 -0600

    Create train class

---
+++ lib/train.rb
@@ -0,0 +1,26 @@
+class Train
+
+  attr_reader :name,
+              :type,
+              :cargo
+
+  def initialize(train_info)
+    @name = train_info[:name]
+    @type = train_info[:type]
+    @cargo = Hash.new(0)
+  end
+
+  def count_cars(car)
+    @cargo[car]
+  end
+
+  def add_cars(car, quantity)
+    @cargo[car] += quantity
+  end
+
+  def weight
+    @cargo.sum do |car, quantity|
+      car.weight * quantity
+    end
+  end
+end
